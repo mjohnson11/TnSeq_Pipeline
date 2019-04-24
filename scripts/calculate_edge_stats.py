ed75a14cbdf0c71b595b6b26528dc8beb1fce113
Milo Johnson <milojohnson@Milos-MacBook-Pro.local>
2019-04-24 11:47:30 -0400

    changed criteria for being 'measured'

--- scripts/calculate_edge_stats.py
+++ scripts/calculate_edge_stats.py
@@ -284,7 +284,7 @@ def analyze_determinants(seg_list, phenos, gm_df, num_subsamples):
 
 
 def get_stats_for_one_edge(row, segs, gm_df, num_subsamples, use_only_two_rep_segs):
-    measured = [seg for seg in segs if row[seg + '.total.cbcs'] >= 3]
+    measured = [seg for seg in segs if pd.notnull(row[seg + '.mean.s'])]  # must have at least 2 cbcs in at least one replicate
     reps = ['rep1', 'rep2']
     stat_dict = {'num.measured': len(measured)}
     if len(measured) > 0:
